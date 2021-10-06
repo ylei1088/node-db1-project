1b7d2746a5e1e78129e1638dedfebae87e460bd2
ylei1088 <ylei1088@gmail.com>
2021-10-06 14:45:58 -0500

    mvp

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,70 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const md = require("./accounts-middleware");
+const Account = require("./accounts-model");
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get("/", async (req, res, next) => {
+  try {
+    const accounts = await Account.getAll();
+    res.json(accounts);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.get("/:id", md.checkAccountId, async (req, res, next) => {
+  // try {
+  //   const account = await Account.getById(req.params.id);
+  //   res.json(account);
+  // } catch (err) {
+  //   next(err);
+  // }
+  res.json(req.account);
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
-})
+router.post(
+  "/",
+  md.checkAccountPayload,
+  md.checkAccountNameUnique,
+  async (req, res, next) => {
+    try {
+      const newAccount = await Account.create({
+        name: req.body.name.trim(),
+        budget: req.body.budget,
+      });
+      res.status(201).json(newAccount);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  md.checkAccountId,
+  md.checkAccountPayload,
+  async (req, res, next) => {
+    try {
+      const updated = await Account.updateById(req.params.id, req.body);
+      res.json(updated);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.delete("/:id", md.checkAccountId, async (req, res, next) => {
+  try {
+    await Account.deleteById(req.params.id);
+    res.json(req.account);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+  });
+});
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,17 @@
 const express = require("express");
 
+const accountRouter = require("./accounts/accounts-router");
+
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/accounts", accountRouter);
+
+server.use("*", (req, res) => {
+  res.status(404).json({
+    message: "not found",
+  });
+});
 
 module.exports = server;
